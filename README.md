e8ffd43e09b8b2252fa69f2faf4d548fe1bb2811
Shipra Behera <shpr13@gmail.com>
2019-04-08 16:30:21 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## 1. How to start
+```
+$ git clone [https://github.com/mkyong/spring-boot.git](https://github.com/mkyong/spring-boot.git)
+$ cd atm-simulator
+$ mvn spring-boot:run
+
+```
---
+++ src/main/java/com/atm/controller/ATMController.java
@@ -0,0 +1,45 @@
+package com.atm.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class ATMController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("View Balance", "Withdraw", "Deposit", "Transfer", "Exit");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+
+    @PostMapping("/login")
+    public String greetingSubmit() {
+        return "menu";
+    }
+
+}
